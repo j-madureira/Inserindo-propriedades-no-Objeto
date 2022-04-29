45782e2a480648ac11edf2e7ac5315e460e8b6e2
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-29 15:07:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//Pessoa2
+var pessoa = {};
+
+pessoa.nome = "João";
+pessoa.idade = "20";
+pessoa.cpf = "31227231";
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.cpf);
+
+//Pessoa2
+var pessoa2 = {};
+pessoa2["nome"] = "Junior";
+pessoa2["idade"] = "23";
+pessoa2["cpf"] = "772313713";
+
+console.log(pessoa2["nome"]);
+console.log(pessoa2["idade"]);
+console.log(pessoa2["cpf"]);
